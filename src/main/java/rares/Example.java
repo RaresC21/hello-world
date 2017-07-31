b060d9d5185301bd4e45404c6c01ab9595dace66
Rares Cristian <raresc2105@gmail.com>
2017-07-30 23:42:27 -0400

    test

---
+++ src/main/java/rares/Example.java
@@ -0,0 +1,8 @@
+package rares;
+
+public class Example {
+	public static void main (String[]  args)  {
+			// TODO
+			// FIXME
+	}
+}
---
+++ src/main/java/rares/HelloWorld.java
@@ -0,0 +1,18 @@
+package rares;
+
+import java.math.BigInteger;
+
+public class HelloWorld {
+	public static void main (String[]  args)  {
+        System.out.println ("Hello World!")
+      	;
+     	int var;
+    	func();
+			// TODO
+			// FIXME
+	}
+
+	public static int func() {
+  		return 0;
+  	}
+}
